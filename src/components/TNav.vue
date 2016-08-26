fa648ff2e14f5de7fc6d3208624d42ed8452d62e
ty888 <1506125048@qq.com>
2016-08-26 15:49:24 +0800

    debug

--- src/components/TNav.vue
+++ src/components/TNav.vue
@@ -1,14 +1,16 @@
 <template>
-<x-header :left-options="{showBack: showBack, backText: ''}" :style="{'background-color':bgcolor}">
+<x-header :right-options="{showMore: true}"  @on-click-more="showMenus = true" :left-options="{showBack: showBack, backText: ''}" :style="{'background-color':bgcolor}">
   <span :style="{'color': textcolor}">{{title}}</span>
 </x-header>
+<actionsheet :menus="menus" @on-click-menu-menu2="showSuccess = true" :show.sync="showMenus" show-cancel></actionsheet>
+<toast :show.sync="showSuccess">退出登录成功!</toast>
 </template>
 
 <script>
-  import { XHeader } from 'vux/src/components/'
+  import { XHeader, Actionsheet, Toast } from 'vux/src/components/'
   export default {
     components: {
-      XHeader
+      XHeader, Actionsheet, Toast
     },
     props: {
       // 导航栏颜色
@@ -24,6 +26,16 @@
       showBack: {
         type: Boolean,
         default: true
+      }
+    },
+    data () {
+      return {
+        menus: {
+          menu1: window.localStorage.uname,
+          menu2: '<span style="color:red">退出登录</span>'
+        },
+        showMenus: false,
+        showSuccess: false
       }
     }
   }

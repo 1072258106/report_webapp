3e694a83c570deb9e6b1004d96b48c1293c18667
tw <t888y@qq.com>
2016-08-26 15:43:04 +0800

    change to the name login model

--- src/CheckInfo.vue
+++ src/CheckInfo.vue
@@ -3,7 +3,7 @@
     <t-nav title="确认报道"></t-nav>
     <group>
       <cell title="学号" :value="userInfo.student_num"></cell>
-      <cell title="姓名" :value="userInfo.name"></cell>
+      <cell title="姓名" :value="userInfo.student_name"></cell>
       <cell title="性别" :value="userInfo.gender_title"></cell>
       <cell title="联系电话" :value="userInfo.tel"></cell>
       <cell title="身份证" :value="userInfo.id_card"></cell>

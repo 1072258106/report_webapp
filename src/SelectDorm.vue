e16db31421cabf5a985873241495b519882b3163
tw1997 <t888y@qq.com>
2016-09-02 15:36:56 +0800

    宿舍删除南北

--- src/SelectDorm.vue
+++ src/SelectDorm.vue
@@ -7,30 +7,13 @@
     <template  v-for="(floorKey, floor) in buildNum">
       <div class="selectdorm_body">
         <header>
-          <span class="directionS">南</span>
           {{unitKey}}单元{{buildNumKey}}栋{{floorKey}}层
-          <span class="directionN">北</span>
         </header>
         <section>
-          <div class="south">
-            <div class="null" v-if="floor.south === undefined"></div>
+          <div class="dorm">
             <ul>
-              <li @click="goSelBed(dorm.id, dorm.dorm_num, dorm.galleryful, dorm.selected_beds, dorm.position)" v-for="dorm in floor.south" :style="{'background-color': dormSColors[$index].bgcolor}">
-                <h4 :style="{ 'color': dormNColors[$index].textcolor }">{{dorm.dorm_num}}</h4>
-                <span class="surplus">剩余:{{dorm.surplus_beds_num}}/{{dorm.galleryful}}</span>
-                <span class="hot"></span>
-              </li>
-            </ul>
-          </div>
-          <div class="aisle">
-            <span class="top">过</span>
-            <span class="bottom">道</span>
-          </div>
-          <div class="north">
-            <div class="null" v-if="floor.north === undefined"></div>
-            <ul>
-              <li @click="goSelBed(dorm.id, dorm.dorm_num, dorm.galleryful, dorm.selected_beds, dorm.position)" v-for="dorm in floor.north" :style="{'background-color': dormNColors[$index].bgcolor}">
-                <h4 :style="{ 'color': dormNColors[$index].textcolor }">{{dorm.dorm_num}}</h4>
+              <li @click="goSelBed(dorm.id, dorm.dorm_num, dorm.galleryful, dorm.selected_beds, dorm.position)" v-for="dorm in floor" :style="{'background-color': dormColors[$index].bgcolor}">
+                <h4 :style="{ 'color': dormColors[$index].textcolor }">{{dorm.dorm_num}}</h4>
                 <span class="surplus">剩余:{{dorm.surplus_beds_num}}/{{dorm.galleryful}}</span>
               </li>
             </ul>
@@ -56,13 +39,11 @@ export default {
   },
   data () {
     return {
-      dormSColors: [
+      dormColors: [
         {bgcolor: '#FBF2A5', textcolor: '#FA8612'},
         {bgcolor: '#CEDEF8', textcolor: '#4F72E6'},
         {bgcolor: '#98FB98', textcolor: '#6CCF6C'},
-        {bgcolor: '#C8EAF3', textcolor: '#327296'}
-      ],
-      dormNColors: [
+        {bgcolor: '#C8EAF3', textcolor: '#327296'},
         {bgcolor: '#FEDCDA', textcolor: '#DA787F'},
         {bgcolor: '#FCA4CE', textcolor: '#FCF2FA'},
         {bgcolor: '#BCD3FC', textcolor: '#3A81D5'},
@@ -117,63 +98,20 @@ export default {
         font-size: .6rem;
         text-align: center;
         padding:.5rem 0;
-        >.directionS{
-          float: left;
-          margin-left: 20px;
-        }
-        >.directionN{
-          float: right;
-          margin-right: 20px;
-        }
       }
       >section{
         position: relative;
         overflow: hidden;
-        >.aisle{
-          position: absolute;
-          width: 20%;
-          top:0;
-          left:50%;
-          transform: translate(-50%,0);
-          height: 100%;
-          >span{
-            position: absolute;
-          }
-          >.top{
-            top:20%;
-            left:50%;
-            transform: translate(-50%,0);
-          }
-          >.bottom{
-            bottom:20%;
-            left:50%;
-            transform: translate(-50%,0);
-          }
-        }
-        >.south,>.north{
-          width: 40%;
-          float: left;
-          >.null{
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            width: 40%;
-            background-color: #f6f6f6;
-            &::after{
-              content: '空';
-              color: #ccc;
-              font-size: 0.7rem;
-              position: absolute;
-              top: 50%;
-              left: 50%;
-              transform: translate(-50%, -50%);
-            }
-          }
+        >.dorm{
           >ul{
             >li{
+              width: 45%;
+              float: left;
               height: 5rem;
-              width: 100%;
               position: relative;
+              &:nth-child(even){
+                float: right;
+              }
               &:not(:last-child){
                 margin-bottom: 10px;
               }
@@ -194,9 +132,6 @@ export default {
               }
             }
           }
-        }
-        >.north{
-          float:right!important;
         }
       }
     }
